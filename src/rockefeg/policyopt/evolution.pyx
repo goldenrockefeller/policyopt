a3c5c76af2be997a00aa63f784272724490aeb9b
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-05-07 01:49:57 -0700

    Add phenotypes to unevaluated phenotype list, when they are added to the total phenotype list

--- src/rockefeg/policyopt/evolution.pyx
+++ src/rockefeg/policyopt/evolution.pyx
@@ -278,18 +278,21 @@ cdef class BaseEvolvingSystem(BaseSystem):
 
     cpdef void append_phenotype(self, phenotype) except *:
         self.__phenotypes.append(<BasePhenotype?>phenotype)
+        self._append_unevaluated_phenotype(phenotype)
 
     cpdef pop_phenotype(self, Py_ssize_t index):
         return self.__phenotypes.pop(index)
 
     cpdef void insert_phenotype(self, Py_ssize_t index, phenotype) except *:
         self.__phenotypes.insert(index, <BasePhenotype?>phenotype)
+        self._insert_unevaluated_phenotype(index, phenotype)
 
     cpdef phenotype(self, Py_ssize_t index):
         return self.__phenotypes[index]
 
     cpdef void set_phenotype(self, Py_ssize_t index, phenotype) except *:
         self.__phenotypes[index] = <BasePhenotype?>phenotype
+        self._set_phenotype(index, phenotype)
 
     cpdef list _phenotypes(self):
         return self.__phenotypes
@@ -333,6 +336,8 @@ cdef class BaseEvolvingSystem(BaseSystem):
                         .format(**locals()) ))
 
         self.__phenotypes = phenotypes
+        self._set_unevaluated_phenotypes(phenotypes)
+
 
     cpdef Py_ssize_t n_unevaluated_phenotypes(self) except *:
           return len(self.__unevaluated_phenotypes)
