1ce6ca1ff3ec6f710975aa53b43a69b14ef5507b
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-07-06 14:39:59 -0700

    Add default jacobian functions

--- src/rockefeg/policyopt/map.pxd
+++ src/rockefeg/policyopt/map.pxd
@@ -1,3 +1,5 @@
+from rockefeg.cyutil.array cimport DoubleArray
+
 cdef class BaseMap:
     cpdef copy(self, copy_obj = ?)
 
@@ -7,5 +9,15 @@ cdef class BaseMap:
     cpdef eval(self, input)
 
 cdef class BaseDifferentiableMap(BaseMap):
-    cpdef grad_wrt_parameters(self, input)
-    cpdef grad_wrt_input(self, input)
+    cpdef jacobian_wrt_parameters(self, input)
+    cpdef jacobian_wrt_input(self, input)
+    cpdef grad_wrt_parameters(self, input, output_grad = ?)
+    cpdef grad_wrt_input(self, input, output_grad = ?)
+
+cpdef list default_jacobian_wrt_parameters(
+    BaseDifferentiableMap map,
+    DoubleArray input)
+
+cpdef list default_jacobian_wrt_input(
+    BaseDifferentiableMap map,
+    DoubleArray input)
--- src/rockefeg/policyopt/map.pyx
+++ src/rockefeg/policyopt/map.pyx
@@ -1,5 +1,7 @@
 cimport cython
 
+from rockefeg.cyutil.array cimport new_DoubleArray
+
 @cython.warn.undeclared(True)
 @cython.auto_pickle(True)
 cdef class BaseMap:
@@ -25,3 +27,56 @@ cdef class BaseDifferentiableMap(BaseMap):
 
     cpdef jacobian_wrt_input(self, input):
         raise NotImplementedError("Abstract method.")
+
+    cpdef grad_wrt_parameters(self, input, output_grad = None):
+        raise NotImplementedError("Abstract method.")
+
+    cpdef grad_wrt_input(self, input, output_grad = None):
+        raise NotImplementedError("Abstract method.")
+
+cpdef list default_jacobian_wrt_parameters(
+        BaseDifferentiableMap map,
+        DoubleArray input):
+    cdef list jacobian
+    cdef Py_ssize_t output_size
+    cdef Py_ssize_t id
+    cdef DoubleArray output_grad
+    cdef object output
+
+    output = map.eval(input)
+    output_size = len(output)
+    jacobian = [None] * output_size
+
+    output_grad = new_DoubleArray(output_size)
+    output_grad.set_all_to(0.)
+
+    for id in range(output_size):
+        output_grad.view[id] = 1.
+        jacobian[id] = map.grad_wrt_parameters(input, output_grad)
+        output_grad.view[id] = 0.
+
+    return jacobian
+
+cpdef list default_jacobian_wrt_input(
+        BaseDifferentiableMap map,
+        DoubleArray input):
+    cdef list jacobian
+    cdef Py_ssize_t output_size
+    cdef Py_ssize_t id
+    cdef DoubleArray output_grad
+    cdef object output
+
+    output = map.eval(input)
+    output_size = len(output)
+    jacobian = [None] * output_size
+
+    output_grad = new_DoubleArray(output_size)
+    output_grad.set_all_to(0.)
+
+    for id in range(output_size):
+        output_grad.view[id] = 1.
+        jacobian[id] = map.grad_wrt_input(input, output_grad)
+        output_grad.view[id] = 0.
+
+
+    return jacobian
